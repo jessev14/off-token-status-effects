1fb4f046d53bb97e14630c2ecb8c34671a0325cd
jessev14 <jessevo321@gmail.com>
2021-06-07 13:10:56 -0400

    Update off-token-status-effects.js

--- scripts/off-token-status-effects.js
+++ scripts/off-token-status-effects.js
@@ -5,13 +5,14 @@ Hooks.once("init", () => {
 });
 
 async function newDrawEffects(wrapped, ...args) {
+    // call original method to create status spirtes and background
     await wrapped.call(this);
 
     // destroy original background and replace with a copy at the same index
     this.effects.children[0]?.destroy();
     const newBG = this.effects.addChildAt(new PIXI.Graphics(), 0).beginFill(0x000000, 0.40).lineStyle(1.0, 0x000000);
 
-    // re-set effect sprite positions and draw new background with new sprite position
+    // re-set effect sprite positions and draw new backgrounds with new sprite position
     const w = Math.round(canvas.dimensions.size / 2 / 5) * 2;
     const nr = Math.floor(this.data.height * 5);
     for (let i = 0; i < this.effects.children.length - 1; i++) {
