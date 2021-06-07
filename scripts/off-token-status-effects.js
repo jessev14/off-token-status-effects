82ba708b57cdd25c3544489ef47bc098331babf2
jessev14 <jessevo321@gmail.com>
2021-06-07 13:09:51 -0400

    Initial commit

---
+++ scripts/off-token-status-effects.js
@@ -0,0 +1,23 @@
+import { libWrapper } from '../lib/shim.js';
+
+Hooks.once("init", () => {
+    libWrapper.register("off-token-status-effects", "Token.prototype.drawEffects", newDrawEffects, "WRAPPER");
+});
+
+async function newDrawEffects(wrapped, ...args) {
+    await wrapped.call(this);
+
+    // destroy original background and replace with a copy at the same index
+    this.effects.children[0]?.destroy();
+    const newBG = this.effects.addChildAt(new PIXI.Graphics(), 0).beginFill(0x000000, 0.40).lineStyle(1.0, 0x000000);
+
+    // re-set effect sprite positions and draw new background with new sprite position
+    const w = Math.round(canvas.dimensions.size / 2 / 5) * 2;
+    const nr = Math.floor(this.data.height * 5);
+    for (let i = 0; i < this.effects.children.length - 1; i++) {
+        const x = (i % nr) * w;
+        const y = Math.floor(i / nr) * w * -1 - w;
+        this.effects.children[i + 1].position.set(x, y);
+        newBG.drawRoundedRect(x + 1, y + 1, w - 2, w - 2, 2);
+    }
+}
